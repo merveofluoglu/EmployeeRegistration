ed6dfd1b2697a62d8cf2cc60d56ca80c025983d6
ofluoglumerve <ofluoglumerve@gmail.com>
2021-08-12 02:49:46 +0300

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using EmployeeRegistration.Models;
+
+namespace EmployeeRegistration.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IConfiguration configuration;
+        public EmployeeController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Add(EmployeeModel employee)
+        {
+            string conStr = configuration.GetConnectionString("DefaultConnectionString");
+            using (SqlConnection connection = new SqlConnection(conStr))
+            {
+                connection.Open();
+                string query = "INSERT INTO EmployeeList(Name, Age, Email, PhoneNumber) VALUES (@Name, @Age, @Email, @PhoneNumber)";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Age", employee.Age);
+                cmd.Parameters.AddWithValue("@Email", employee.Email);
+                cmd.Parameters.AddWithValue("@PhoneNumber", employee.PhoneNumber);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            return Json(true);
+        }
+
+        [HttpGet]
+        public JsonResult GetEmployeeList()
+        {
+            List<EmployeeModel> list = new List<EmployeeModel>();
+            string conStr = configuration.GetConnectionString("DefaultConnectionString");
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+
+                string query = "SELECT * FROM EmployeeList";
+
+                var _dataTable = new DataTable();
+
+                using SqlDataAdapter _adapter = new SqlDataAdapter(query, con);
+
+                _adapter.Fill(_dataTable);
+
+                list = _dataTable.AsEnumerable().
+                    Select(x => new EmployeeModel
+                    {
+                        Age = Convert.ToInt32(x["Age"]),
+                        Email = x["Email"].ToString(),
+                        EmployeeId = Convert.ToInt32(x["EmployeeId"]),
+                        Name = x["Name"].ToString(),
+                        PhoneNumber = x["PhoneNumber"].ToString(),
+                    }).
+                    ToList();
+            }
+
+            return Json(list);
+        }
+        
+        [Route("employee/delete/{_id}")]
+        public JsonResult Delete(long _id)
+        {
+            string conStr = configuration.GetConnectionString("DefaultConnectionString");
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+                string query = "DELETE FROM EmployeeList WHERE EmployeeId=@id";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.Add("@id", SqlDbType.Int).Value = _id;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return Json(true);
+        }
+
+        [Route("employee/edit")]
+        [HttpPost]
+        public JsonResult Edit(EmployeeModel emp)
+        {
+
+            string conStr = configuration.GetConnectionString("DefaultConnectionString");
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+                string query = "UPDATE EmployeeList SET Name=@Name, Age=@Age, Email=@Email, PhoneNumber=@PhoneNumber WHERE EmployeeId=@_id";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.Add("@_id", SqlDbType.Int).Value = emp.EmployeeId;
+                    cmd.Parameters.Add("@Name", SqlDbType.NVarChar).Value = emp.Name;
+                    cmd.Parameters.Add("@Age", SqlDbType.Int).Value = emp.Age;
+                    cmd.Parameters.Add("@Email", SqlDbType.NVarChar).Value = emp.Email;
+                    cmd.Parameters.Add("@PhoneNumber", SqlDbType.NVarChar).Value = emp.PhoneNumber;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return Json(true);
+        }
+    }
+}
---
+++ wwwroot/js/main.js
@@ -0,0 +1,156 @@
+﻿
+
+const addEmployee = () => {
+    const _data = {
+        EmployeeId: parseInt($("#addEmployee [name='EmployeeId']").val()),
+        Name: $("#addEmployee [name='Name']").val(),
+        Age: parseInt($("#addEmployee [name='Age']").val()),
+        Email: $("#addEmployee [name='Email']").val(),
+        PhoneNumber: $("#addEmployee [name='PhoneNumber']").val()
+    };
+    $.ajax({
+        url: "/employee/add/",
+        method: "POST",
+        data: _data,
+        success: function (response) {
+            $('#addEmployee').modal('hide');
+            var table = $('#EmployeeList').DataTable();
+            table.destroy();
+            FillDatatable();
+            toastr.success("Employee added succesfully!");
+        },
+        error: function () {
+            alert("error");
+        }
+    }
+    );
+}
+
+const removeEmployee = (id) => {
+    $.ajax({
+        url: "/employee/delete/" + id,
+        method: "GET",
+        success: function (response) {
+            var table = $('#EmployeeList').DataTable();
+            table.destroy();
+            FillDatatable();
+            toastr.error("Employee deleted!");
+        },
+        error: function () {
+            alert("error");
+        }
+    }
+    );
+}
+
+const updateEmployee = () => {
+
+    const _data = {
+        EmployeeId: parseInt($("#editEmployee [name='EmployeeId']").val()),
+        Name: $("#editEmployee [name='Name']").val(),
+        Age: parseInt($("#editEmployee [name='Age']").val()),
+        Email: $("#editEmployee [name='Email']").val(),
+        PhoneNumber: $("#editEmployee [name='PhoneNumber']").val()
+    };
+
+    $.ajax({
+        url: "/employee/edit",
+        method: "post",
+        data: _data,
+        success: function (response) {
+            $('#editEmployee').modal('hide');
+            var table = $('#EmployeeList').DataTable();
+            table.destroy();
+            FillDatatable();
+            toastr.info("Employee updated succesfully!");
+        },
+        error: function () {
+            alert("error");
+        }
+    }
+    );
+}
+
+const FillDatatable = () => {
+
+    let _selectedId = 0;
+    let _selectedName;
+    let _selectedAge;
+    let _selectedEmail;
+    let _selectedPhoneNum;
+
+    $.ajax({
+        url: '/Employee/GetEmployeeList',
+        method: "GET",
+        dataType: 'json',
+        success: function (data) {
+
+            $('#EmployeeList').DataTable({
+                data: data,
+                dom: "Bfrtip",
+                columns: [
+                    { title: "Employee Id", data: "employeeId" },
+                    { title: "Name", data: "name" },
+                    { title: "Age", data: "age" },
+                    { title: "Email", data: "email" },
+                    { title: "Phone Number", data: "phoneNumber" }
+                ],
+                select: true,
+                buttons: [{
+                    text: "Delete",
+                    atr: {
+                        id: 'delete'
+                    },
+                    action: function () {
+                        if (_selectedId == 0)
+                            alert("Please select a row!");
+                        else {
+                            $("#dialog").modal('show');
+
+                            $("#confirm").click(function () {
+                                $('#dialog').modal('hide');
+                                removeEmployee(_selectedId);
+                            });
+                        }
+                    }       
+                },
+                {
+                    text: "Edit",
+                    atr: {
+                        id: 'edit'
+                    },
+                    action: function () {
+                        if (_selectedId == 0)
+                            alert("Please select a row!");
+                        else {
+                            $("#editEmployee [name='EmployeeId']").val(_selectedId);
+                            $("#editEmployee [name='Name']").val(_selectedName);
+                            $("#editEmployee [name='Age']").val(_selectedAge);
+                            $("#editEmployee [name='Email']").val(_selectedEmail);
+                            $("#editEmployee [name='PhoneNumber']").val(_selectedPhoneNum);
+                            $("#editEmployee").modal('show');
+                        }
+                    }
+                },
+                {
+                    text: "Add Employee",
+                    atr: {
+                        id: 'add'
+                    },
+                    action: function () {                                          
+                        $("#addEmployee").modal('show');
+                    }
+                }
+                ]
+            }).off("select")
+            .on("select", function (e, dt, type, indexes) {
+                _selectedId = dt.data().employeeId;
+                _selectedName = dt.data().name;
+                _selectedAge = dt.data().age;
+                _selectedEmail = dt.data().email;
+                _selectedPhoneNum = dt.data().phoneNumber;
+                });
+        }
+    });
+
+}
